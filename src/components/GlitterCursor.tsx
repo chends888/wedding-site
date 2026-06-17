7b3d4cd5d59fa51e4c1b5ace787bf8801be451b1
Lucas Chen <lchends@gmail.com>
2026-06-17 00:59:14 -0300

    Fix for Vercel deploy on GlitterCursor

--- src/components/GlitterCursor.tsx
+++ src/components/GlitterCursor.tsx
@@ -79,7 +79,6 @@ export default function GlitterCursor() {
     return () => {
       window.removeEventListener('mousemove', onMouseMove)
       window.removeEventListener('touchmove', onTouchMove)
-      cancelAnimationFrame(frameId)
       style.remove()
     }
   }, [])
